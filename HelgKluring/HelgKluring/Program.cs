5f55f940dedf379b53ee9a9ab7ae16dcfe06aeb9
Tarfala <jon.risberg@gmail.com>
2019-03-29 11:27:04 +0100

    Konsolapp som retunerar antal siffror i en int utan att parsa till string

---
+++ HelgKluring/HelgKluring/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HelgKluring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) // Ta bort loopen ifall programmet endast ska köras 1 gång
+            {
+                RunProgram();
+            }            
+        }
+
+        private static void RunProgram()
+        {
+            int? input = GetInputFromUser(); // Tar fram, och validerar det användaren skriver in
+            if (input != null)
+            {
+                Console.WriteLine("Antal siffror i talet är: " + NumDigits((int)input));
+            }
+
+            Console.WriteLine("Klicka på valfri knapp för att testa igen");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    
+
+        private static int? GetInputFromUser()
+        {
+            Console.WriteLine("Ange ett nummer och klicka på enter:");
+            try
+            {
+                int input = int.Parse(Console.ReadLine());
+                return input;
+            }
+            catch (Exception) // Om parsen inte funkar returneras null 
+            {
+                Console.WriteLine("Du måste ange ett heltal (max +/- 2,147,483,647), klicka på valfri knapp för att försöka igen");
+                return null;
+            }
+        }
+
+        private static int NumDigits(int input)
+        {
+            int numberOfDidgits = 0;
+            if (input == 0) // Ta bort denna If-satsen ifall talet 0 inte ska räknas som en siffra (0 returnerar i så fall 0)
+            {
+                return 1;
+            }
+
+            while (input != 0)
+            {
+                numberOfDidgits++;
+                input /= 10;
+            }            
+
+            return numberOfDidgits;
+        }
+    }
+}
